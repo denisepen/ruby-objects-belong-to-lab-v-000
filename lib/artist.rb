6b35a5b1ab1023b343c0035ed55757ede30552d8
Denise Pendleton <dpendl1@hotmail.com>
2018-03-04 22:59:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,8 +3,14 @@ class Artist
   attr_accessor :name, :song
   def initialize(name)
     @name = name
-
   end
+
+  class Song
+    attr_accessor :title, :artist
+
+    def initialize(title)
+      @title = title
+    end
 
 end
   
